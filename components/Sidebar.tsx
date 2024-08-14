54cdd19b3d7ba30938e429f5358e7b09b53c6b60
Ammar-Amin <ammaramin5253@gmail.com>
2024-08-14 14:15:23 +0530

    add side bar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,37 @@
+"use client";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Sidebar = () => {
-  return <h1 className="w-[270px] min-h-screen bg-slate-900">Sidebar</h1>;
+  const path = usePathname();
+  return (
+    <div className="sticky left-0 top-0 h-screen w-fit flex flex-col justify-between bg-slate-900 p-6 pt-28 text-white max-sm:hidden lg:w-[270px]">
+      <div className="flex flex-col gap-6">
+        {sidebarLinks.map((link, i) => {
+          const isActive = path === link.path;
+
+          return (
+            <Link
+              key={i}
+              href={link.path}
+              className={cn(
+                "flex gap-4 justify-start items-center p-4 rounded-lg lg:text-lg",
+                {
+                  "bg-blue-600": isActive,
+                }
+              )}
+            >
+              <span>{link.img}</span>
+              <span>{link.label}</span>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    label: "Home",
+    path: "/",
+    img: "#",
+  },
+  {
+    label: "Upcoming",
+    path: "/upcoming",
+    img: "#",
+  },
+  {
+    label: "Previous",
+    path: "/previous",
+    img: "#",
+  },
+  {
+    label: "Recordings",
+    path: "/recordings",
+    img: "#",
+  },
+  {
+    label: "Personal Room",
+    path: "/personal-room",
+    img: "#",
+  },
+];

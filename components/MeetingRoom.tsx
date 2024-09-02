8f4c7b2527d70e50267411b32d1de7202e84789e
Ammar-Amin <ammaramin5253@gmail.com>
2024-09-02 10:54:32 +0530

    install next@latest to avoid errors, add meeting setup component with a custom hook

---
+++ components/MeetingRoom.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MeetingRoom = () => {
+  return <div className="w-full h-screen">Meeting Room</div>;
+};
+
+export default MeetingRoom;
---
+++ components/MeetingSetup.tsx
@@ -0,0 +1,60 @@
+"use client";
+import {
+  DeviceSettings,
+  useCall,
+  VideoPreview,
+} from "@stream-io/video-react-sdk";
+import React, { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+
+const MeetingSetup = ({
+  isSetupComplete,
+}: {
+  isSetupComplete: (value: boolean) => void;
+}) => {
+  const [isMicCameraOn, setIsMicCameraOn] = useState(false);
+
+  const call = useCall();
+
+  if (!call) {
+    throw new Error("useCall must be use inside StreamCall Component");
+  }
+  useEffect(() => {
+    if (isMicCameraOn) {
+      call?.camera.enable();
+      call?.microphone.enable();
+    } else {
+      call?.camera.disable();
+      call?.microphone.disable();
+    }
+  }, [isMicCameraOn, call?.camera, call?.microphone]);
+
+  return (
+    <div className="w-full h-screen flex-center flex-col gap-3 text-white">
+      <h1 className="font-semibold text-2xl">Setup</h1>
+      <VideoPreview />
+      <div className="flex-center gap-3 h-16">
+        <label className="flex-center gap-2 font-medium">
+          <input
+            type="checkbox"
+            checked={isMicCameraOn}
+            onChange={(e) => setIsMicCameraOn(e.target.checked)}
+          />
+          Join with Mic & Camera On
+        </label>
+        <DeviceSettings />
+      </div>
+      <Button
+        className="rounded-lg bg-green-700 text-white"
+        onClick={() => {
+          call.join();
+          isSetupComplete(true);
+        }}
+      >
+        Join Meeting
+      </Button>
+    </div>
+  );
+};
+
+export default MeetingSetup;
